8b7e331ca864251cd02f3ab6cb8cd7afab596cc9
gabao55 <gabriel.s.rosin@gmail.com>
2022-09-29 08:16:19 -0300

    Connects to Postgres

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connection from './db/db.js';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/status', async (req, res) => {
+    const test = await connection.query('SELECT * FROM games;');
+    console.log(test.rows);
+    res.sendStatus(200);
+});
+
+app.listen(process.env.PORT, console.log(`Listening to PORT ${process.env.PORT}`));
